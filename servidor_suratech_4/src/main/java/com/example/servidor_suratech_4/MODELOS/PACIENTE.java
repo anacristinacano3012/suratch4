9ae6fb0cde590fb3a8d7b830bc7c58d55a8ac810
anacano <ana.cano3012@gmail.com>
2024-09-17 15:08:45 -0500

    jsonteamo

---
+++ servidor_suratech_4/src/main/java/com/example/servidor_suratech_4/MODELOS/PACIENTE.java
@@ -0,0 +1,28 @@
+package com.example.servidor_suratech_4.MODELOS;
+
+import java.time.LocalDate;
+
+public class PACIENTE {
+    private Integer id;
+
+    private String nombres;
+
+    private Integer edad;
+
+    private LocalDate fechaNacimiento;
+
+    private String documento;
+
+    private String telefono;
+
+    private Integer copago;
+
+    private String email;
+
+    private String direccion;
+
+    private String eps;
+
+    private String medicoFamilia;
+
+}
